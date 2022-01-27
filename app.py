6f8fd114f5cbaa17342feb5c6cb90a756d6ad24d
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-01-26 20:30:52 -0500

    Working on further study

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
 debug = DebugToolbarExtension(app)
-
+chosen_story = None
 # change homepage to select which story
 # submitting selected story should do get with story name to /questions?store-name=silly-story
 # /question view look up the story from a global STORIES dictionary
@@ -20,17 +20,31 @@ STORIES = {"silly story": silly_story, "excited story": excited_story}
 
 
 @app.get("/")
-def home():
-    """Home page of Madlib"""
+def story_type():
+    """Page to choose the story template"""
 
-    return render_template("questions.html", prompts=silly_story.prompts)
+    return render_template("chooseStory.html", storynames=STORIES.keys())
+
+    
+
+@app.get("/story")
+def question():
+    """Question page to recieve input for completing story from user """
+    global story_name
+    story_name = request.args.get("story-name")
+    chosen_story = STORIES[story_name]
+
+
+    return render_template("questions.html", prompts=chosen_story.prompts)
+
+
 
 
 @app.get("/results")
 def story_result():
     """Story page with submitted words"""
 
-    text = silly_story.generate(request.args)
+    text = chosen_story.generate(request.args)
 
     return render_template("story.html", story=text)
 
